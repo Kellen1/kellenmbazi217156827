db191be5e0c9e7ba324d9dd0903c4eb86c49de06
mbabazi <mbabazikellen2016@gmail.com>
2019-01-31 13:32:30 +0200

    first commit

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -62,11 +62,8 @@ class StudentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_student
-      @student = Student.find(params[:id])
-    end
-
+   @student= Student.find (params[:id]) 
     # Never trust parameters from the scary internet, only allow the white list through.
     def student_params
       params.require(:student).permit(:regno, :name, :telephone, :address)
